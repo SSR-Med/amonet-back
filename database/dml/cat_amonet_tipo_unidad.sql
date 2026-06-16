8259356cfede8c79ffcedae4c58fb35d910a88a9
Santiago <sasaravi@gmail.com>
2026-06-16 10:37:33 -0500

    Update DML: replace KG/LT with G/ML in unidad, add CALIDAD role

--- database/dml/cat_amonet_tipo_unidad.sql
+++ database/dml/cat_amonet_tipo_unidad.sql
@@ -1,4 +1,3 @@
-INSERT INTO cat_amonet_tipo_unidad (nombre, abreviacion) VALUES ('KILOGRAMOS', 'KG');
-INSERT INTO cat_amonet_tipo_unidad (nombre, abreviacion) VALUES ('LITROS', 'LT');
+INSERT INTO cat_amonet_tipo_unidad (nombre, abreviacion) VALUES ('GRAMOS', 'G');
+INSERT INTO cat_amonet_tipo_unidad (nombre, abreviacion) VALUES ('MILILITROS', 'ML');
 INSERT INTO cat_amonet_tipo_unidad (nombre, abreviacion) VALUES ('UNIDADES', 'UD');
-INSERT INTO cat_amonet_tipo_unidad (nombre, abreviacion) VALUES ('GRAMOS','G')
--- database/dml/cat_amonet_usuario_rol.sql
+++ database/dml/cat_amonet_usuario_rol.sql
@@ -1,2 +1,3 @@
 INSERT INTO cat_amonet_usuario_rol (nombre) VALUES ('ADMIN');
 INSERT INTO cat_amonet_usuario_rol (nombre) VALUES ('OPERARIO');
+INSERT INTO cat_amonet_usuario_rol (nombre) VALUES ('CALIDAD');
